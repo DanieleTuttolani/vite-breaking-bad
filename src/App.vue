83f61bb8ff5daa10b4db3f90f3cb216e04c89326
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-24 17:05:03 +0100

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import { store } from './assets/data/store';
+import CardSection from './components/CardSection.vue';
+export default {
+    data (){
+        return{
+            store
+        }
+    },
+    components :{
+        CardSection
+    },
+    created() {
+        axios.get(this.store.pokeUri)
+        .then(res =>{
+            this.store.pokemons = res.data.docs
+        })
+    }
+}
 </script>
 
 <template>
-  <div class="container"></div>
+  <div class="container">
+        <div class="main-title mt-5">
+            <h1 class=" text-center text-light">Il Mio Pokedex</h1>
+        </div>
+        <!-- card section -->
+        <CardSection/>
+  </div>
 </template>
 
 <style lang="scss">
-@use './assets/scss/style.scss'
+@use './assets/scss/style.scss'as *;
+h1{
+    text-shadow: black 0 0 3px;
+}
 </style>
---
+++ src/components/CardSection.vue
@@ -0,0 +1,58 @@
+<script>
+import { store } from '../assets/data/store';
+
+export default{
+    data (){
+        return{
+            store
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="row row-cols-4 cs-card-wrapper p-3">
+            <div v-for="pokemon in store.pokemons" :key="pokemon.id" class="card-wrapper my-3">
+                <div class="card text-light" :class="pokemon.color" style="width: 18rem;">
+                    <img :src="pokemon.imageUrl" class="card-img-top"  alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ pokemon.name }}</h5>
+                        <p class="card-text"> Pokemon HP :{{ pokemon.hp }}</p>
+                        <p class="card-text"> Pokemon ATK :{{ pokemon.atk }}</p>
+                        
+                    </div>
+                </div>
+            </div>
+            
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.cs-card-wrapper{
+    min-height: 500px;
+    background-color: rgb(223, 223, 223);
+    border-radius: 30px;
+    .card-img-top{
+        height: 280px;
+        width: 286px;
+    }
+    .Green{
+        background-color: green;
+    }
+    .Red{
+        background-color: darkred;
+    }
+    .Blue{
+        background-color: dodgerblue;
+    }
+    .Brown , .Purple{
+        background-color: gray;
+    }
+    .Pink{
+        background-color: pink;
+    }
+}
+</style>
