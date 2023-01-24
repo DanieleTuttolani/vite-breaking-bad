1d75417db1528b473ed9fc0675d4a91e732c57a3
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-24 14:19:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss'
+</style>
